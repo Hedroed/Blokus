9476ce7ff8fc11241ef31dda07de285c6a86d662
Hedroed <nathan.rydin@gmail.com>
2016-05-10 16:48:48 +0200

    Javadoc sur IA et Joueur

--- src/joueur/Joueur.java
+++ src/joueur/Joueur.java
@@ -3,6 +3,11 @@ package joueur;
 import java.util.*;
 import blokus.*;
 
+/**
+  * Le Joueur contient tout les parametres d'un joueur durant une partie de Blokus
+  * Une liste de pieces, une couleur et un nom
+  * Il permet de savoir quel joueur est en train de jouer.
+  */
 public class Joueur {
 
 	private Collection<Piece> pieces;
@@ -11,7 +16,7 @@ public class Joueur {
 	private boolean peutJouer;
 
 	/**
-	 * 
+	 * Constructeur prend un nom et une couleur
 	 * @param nom
 	 * @param couleur
 	 */
@@ -19,15 +24,24 @@ public class Joueur {
 		// TODO - implement Joueur.Joueur
 		throw new UnsupportedOperationException();
 	}
-
+	
+	/**
+	  * Initalise les pieces en debut de partie
+	  * est appeler par le constructeur
+	  */
 	private void creePieces() {
 		// TODO - implement Joueur.creePieces
 		throw new UnsupportedOperationException();
 	}
 	
-	public void isIA() {
+	/**
+	  * Permet de savoir si ce joueur est une IA pour appeler la method placePiece de l'IA
+	  * @return un boolean
+	  */
+	public boolean isIA() {
 		// TODO - implement Joueur.isIA
 		throw new UnsupportedOperationException();
+		return false;
 	}
 
 }
